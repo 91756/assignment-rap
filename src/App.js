8027eb5b7a8e3aec1f72c5eb97b351a805d8457f
krishna <krishna28@gmail.com>
2022-08-20 08:17:18 +0000

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import Home from './components/Home'
-import AddResource from './components/AddResource'
-import LoginForm from './components/LoginForm'
+import {useState, useEffect} from 'react'
 
-const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/login" component={LoginForm} />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/add-resources" component={AddResource} />
-    </Switch>
-  </BrowserRouter>
-)
+const App = () => {
+  const [count, setCount] = useState(0)
+  const [data, setData] = useState([
+    {
+      name: 'krisna',
+    },
+    {
+      name: 'supriya',
+    },
+  ])
+  console.log(count)
+  console.log(data)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(dat => {
+        setData(dat)
+      })
+    return () => {
+      console.log('unmount')
+    }
+  }, [count])
+
+  return (
+    <div style={{textAlign: 'center'}}>
+      {data.map(user => (
+        <p key={user.name}>{user.name}</p>
+      ))}
+      <h1>{count}</h1>
+      <button
+        type="button"
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        Increment count
+      </button>
+    </div>
+  )
+}
 export default App
